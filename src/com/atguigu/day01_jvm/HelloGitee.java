b7d92084e0b2239b553a8e04ff5df929dfffd017
ADun <2101580047@qq.com>
2019-12-18 20:04:14 +0800

    update src/com/atguigu/day01_jvm/HelloGitee.java.

--- src/com/atguigu/day01_jvm/HelloGitee.java
+++ src/com/atguigu/day01_jvm/HelloGitee.java
@@ -2,7 +2,7 @@ package com.atguigu.day01_jvm;
 
 public class HelloGitee {
     public static void main(String[] args) {
-        System.out.println("Hello Gitee!");
+        System.out.println("Hello Gitee!hei hei");
     }
 
 }
