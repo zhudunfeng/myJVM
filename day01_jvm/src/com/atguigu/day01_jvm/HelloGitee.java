a7630f69c676994c23d65f2004d138f755a9b29c
朱敦峰 <2101580047@qq.com>
2019-12-19 11:28:00 +0800

    冲突修改1

--- day01_jvm/src/com/atguigu/day01_jvm/HelloGitee.java
+++ day01_jvm/src/com/atguigu/day01_jvm/HelloGitee.java
@@ -3,6 +3,8 @@ package com.atguigu.day01_jvm;
 public class HelloGitee {
     public static void main(String[] args) {
         System.out.println("Hello Gitee！哈哈，呀呀");
+        System.out.println("Hello Gitee！哈哈");
+        System.out.println("Hello Gitee！哈哈,嘿嘿");
         System.out.println("Hello Gitee!heihei");
     }
 
