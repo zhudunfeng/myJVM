584d0ed9f29b705862a69dbbae2d6c634d578001
zhudunfeng <2101580047@qq.com>
2020-08-26 23:21:03 +0800

    栈溢出异常demo

--- day01_jvm/src/com/atguigu/day01_jvm/StackOverflowDemo.java
+++ day01_jvm/src/com/atguigu/day01_jvm/StackOverflowDemo.java
@@ -5,13 +5,15 @@ public class StackOverflowDemo {
 
     //递归调用,没有结束条件，会栈溢出
     public static void test(){
-        if(i==10){
-            return;
-        }
+//        if(i==10){
+//            return;
+//        }
         System.out.println(i);
         i++;
-        new StackOverflowDemo().test();
+//        new StackOverflowDemo().test();
+        test();
     };
+
 
     public static void main(String[] args) {
         test();
